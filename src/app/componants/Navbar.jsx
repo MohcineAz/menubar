8d992f8bfe74fa325498c1f9b4356111e0d3e295
Mohcine <olcfuturtech@gmail.com>
2025-06-23 12:40:07 +0100

    Add items for menu

--- src/app/componants/Navbar.jsx
+++ src/app/componants/Navbar.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+const MenuBarList = [
+  {
+    id: 1,
+    title: "Inbox",
+    link: "#inbox",
+  },
+  {
+    id: 2,
+    title: "Sent",
+    link: "#sent",
+  },
+  {
+    id: 3,
+    title: "Drafts",
+    link: "#drafts",
+  },
+  {
+    id: 4,
+    title: "Important",
+    link: "#important",
+  },
+  {
+    id: 5,
+    title: "Spam",
+    link: "#spam",
+  },
+  {
+    id: 6,
+    title: "Trash",
+    link: "#trash",
+  },
+];
 function Navbar() {
   return (
-    <div>
-      Navbarmenu
+    <div className="max-w-[350px] bg-white p-6 rounded-2xl">
+      <ul>
+        {
+        MenuBarList.map((menu) => (
+          <li key={menu.id}>
+            <a href={menu.link}>
+              {menu.title}
+            </a>
+          </li>
+        ))
+        }
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
